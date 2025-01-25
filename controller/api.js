f65359f4e7e10463db605fce7984e2c6ec9b812c
AADIL812 <aadilharis812@gmail.com>
2025-01-25 17:31:50 +0530

    Jan25'2025'4

--- controller/api.js
+++ controller/api.js
@@ -1,9 +1,7 @@
 const {UserRouter}=require('./Users.router');
 const express=require('express');
 const api=express.Router();
-api.use('/',async (req,res)=>{
-    res.send('Welcome to the API');
-});
+
 api.use('/users',UserRouter);
 
 module.exports={api};
